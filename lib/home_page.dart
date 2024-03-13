fd2b8dc0ed26e5e595cd8978df1c46d5e35146db
ritz89 <shs500@umkt.ac.id>
2024-03-13 14:03:52 +0800

    container and box decoration

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,13 +5,35 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-            child: Text(
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          children: [LeafBorderText(), LeafBorderText(), LeafBorderText()],
+        ),
+      ),
+    );
+  }
+}
+
+class LeafBorderText extends StatelessWidget {
+  const LeafBorderText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.blue,
+          border: Border.all(),
+          borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(20), topRight: Radius.circular(20))),
+      child: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text(
           "Halo Dunia",
           style: TextStyle(color: Colors.amber, fontWeight: FontWeight.bold),
-        )),
+        ),
       ),
     );
   }
